102c63775faeebdf23803cf834620b5566180d19
Tu Nombre <you@example.com>
2018-07-26 18:36:45 -0500

    octavo

--- assets/js/main/frags/servicios.js
+++ assets/js/main/frags/servicios.js
@@ -3,8 +3,8 @@ var app = angular.module('myapp');
 app.controller('serviciosCtrl', function($scope, $rootScope, $http, $mdDialog, mdDialog, $timeout, $mdSidenav, $state, $stateParams, Servicio, Imagen, Item) {
 
 	var self = this;
-	var bandera = 0;
 	self.muestra = false;
+	self.muestra2 = true;
 
 	class Servicios_{
 		constructor(){
@@ -15,13 +15,16 @@ app.controller('serviciosCtrl', function($scope, $rootScope, $http, $mdDialog, m
 
 		obtener(){
 			Servicio.obtener()
-			.then(res => res.data.map(n =>  new Servicio_(n)))
+			.then(res => res.data.map(n => new Servicio_(n)))
 			.then(res => this.agregarServicio(res))
 		}
 
 		agregarServicio(array){
 			array.forEach(n => this.items.push(n))
 			$scope.$digest()
+			console.log(self.servicios.items)
+			this.obtenerMasInfo(0);
+
 			$('.slider').slick({
 				infinite: true,
 				speed: 300,
@@ -32,37 +35,41 @@ app.controller('serviciosCtrl', function($scope, $rootScope, $http, $mdDialog, m
 				nextArrow: $('.next'),
 			})
 			$('.slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
-				//var elSlide = $(slick.$slides[currentSlide]);
-				//var dataIndex = elSlide[0].dataset.slickIndex;
-				self.servicios.item = {}
+				let elSlide = $(slick.$slides[currentSlide]);
+				let dataIndex = elSlide[0].dataset.slickIndex;
 				var detalles = ($('.detalles-servicios'))
-				TweenLite.to(detalles, .2, {width:'0%', height:'0%'})
-				self.muestra = false;
+				TweenLite.to(detalles, .2, {width:'0%', height:'100%'})
+				this.item = {};
+				$scope.$digest()
+				self.servicios.obtenerMasInfo(dataIndex);
 			});
-			console.log(self.servicios.items)
+
 		}
 
-		infoSlider(id){
-			
-			self.muestra = true;
-			self.servicios.item = servicio;
+		obtenerMasInfo(idx){
+			this.item = self.servicios.items[idx]
+			$scope.$digest()
+			console.log(self.servicios.item)
+			//Servicio.obtenerSub(self.servicios.items[idx].id)
+		}
+
+		async abrirDetalles(subservicio){
+			self.subservicio = subservicio;
+			self.muestra2 = false;
 			var detalles = ($('.detalles-servicios'))
 			TweenLite.to(detalles, .3, {width:'50%', height:'100%'})
+			await Item.obtenerDeSubservicios(subservicio.id)
+			.then(res => self.subservicio.elementos = res.data)
+			.then(() => $scope.$digest())
 
-			$scope.$digest()
+			await Imagen.obtenerDesubservicios(this.id)
+			.then(res =>{ self.subservicio.imagenes = res.data; console.log(self.subservicio)})
+			.then(() => $scope.$digest())
+
+
+
 		}
 
-		verInfoSubservicio(subservicio){
-			$('.algo').slick({
-				infinite: true,
-				speed: 300,
-				slidesToShow: 1,
-				adaptiveHeight: true
-			})
-			self.servicios.detalles = subservicio;
-			console.log(self.servicios.detalles)
-			$scope.$digest()
-		}
 	}
 
 	self.servicios = new Servicios_();
@@ -72,17 +79,15 @@ app.controller('serviciosCtrl', function($scope, $rootScope, $http, $mdDialog, m
 			this.id = arg.id,
 			this.nombre = arg.nombre,
 			this.descripcion = arg.descripcion,
-			this.subservicios = [],
-			this.imagenes()
+			this.imagenes(),
+			this.obtenerSubservicios()
 		}
 
-		cargarSubservicios(servicio){
-			Servicio.obtenerSub(servicio.id)
+		async obtenerSubservicios(){
+
+			await Servicio.obtenerSub(this.id)
 			.then(res => this.subservicios = res.data)
-			.then(res => self.servicios.item = servicio)
 			.then(() => $scope.$digest())
-			self.muestra = true;
-			TweenLite.to($('.detalles-servicios'), .3, {width:'50%', height:'100%'})
 		}
 
 		async imagenes(){
@@ -91,5 +96,4 @@ app.controller('serviciosCtrl', function($scope, $rootScope, $http, $mdDialog, m
 			.then(() => $scope.$digest())
 		}
 	}
-
 });
